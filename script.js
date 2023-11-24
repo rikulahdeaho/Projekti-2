2e77bd99a0dc16981ab8a25b63159e30ff1fbbbb
ipu1 <112542319+ipu1@users.noreply.github.com>
2023-11-24 09:28:52 +0000

    checkbox

--- script.js
+++ script.js
@@ -1,25 +1,42 @@
 const getJokeBtn = document.getElementById('getJokeBtn');
 const jokeSetup = document.getElementById('jokeSetup');
 const jokeDelivery = document.getElementById('jokeDelivery');
-const jokeCategorySelect = document.getElementById('jokeCategory');
 
 getJokeBtn.addEventListener('click', () => {
-  const selectedCategory = jokeCategorySelect.value;
+  const programmingCheckbox = document.getElementById('programmingCheckbox');
+  const christmasCheckbox = document.getElementById('christmasCheckbox');
 
-  fetch(`https://v2.jokeapi.dev/joke/${selectedCategory}?blacklistFlags=nsfw,racist,sexist&type=twopart`)
-    .then(response => response.json())
-    .then(data => {
-      if (data.error) {
-        jokeSetup.textContent = `Failed to fetch ${selectedCategory} joke. Please try again.`;
+  // Build the categories based on selected checkboxes
+  const selectedCategories = [];
+  if (programmingCheckbox.checked) {
+    selectedCategories.push('Programming');
+  }
+  if (christmasCheckbox.checked) {
+    selectedCategories.push('Christmas');
+  }
+
+  // Check if at least one category is selected
+  if (selectedCategories.length > 0) {
+    const apiUrl = `https://v2.jokeapi.dev/joke/${selectedCategories.join()}?blacklistFlags=nsfw,racist,sexist&type=twopart`;
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        if (data.error) {
+          jokeSetup.textContent = `Failed to fetch joke. Please try again.`;
+          jokeDelivery.textContent = '';
+        } else {
+          jokeSetup.textContent = data.setup;
+          jokeDelivery.textContent = data.delivery;
+        }
+      })
+      .catch(error => {
+        jokeSetup.textContent = 'An error occurred while fetching the joke.';
         jokeDelivery.textContent = '';
-      } else {
-        jokeSetup.textContent = data.setup;
-        jokeDelivery.textContent = data.delivery;
-      }
-    })
-    .catch(error => {
-      jokeSetup.textContent = 'An error occurred while fetching the joke.';
-      jokeDelivery.textContent = '';
-      console.error('Error fetching joke:', error);
-    });
+        console.error('Error fetching joke:', error);
+      });
+  } else {
+    jokeSetup.textContent = 'Please select at least one category.';
+    jokeDelivery.textContent = '';
+  }
 });
